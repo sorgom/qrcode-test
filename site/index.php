21d07139ddf41b1dfd62dae111fbbcc70e7ea7fa
sorgom <110917257+sorgom@users.noreply.github.com>
2026-07-16 22:35:26 +0200

    a bit closer

--- site/index.php
+++ site/index.php
@@ -1,13 +1,14 @@
+<!DOCTYPE html>
 <?php 
     require_once('my_url.php');
     $url = my_url();
     $png = 'qrcode.png';
-    // if (!file_exists($png))
-    if (true)
+    if (!file_exists($png))
+    // if (true)
     {
         require_once('php-qrcode/qrcode.php');
         $options = [
-            'sf' => 4,
+            // 'sf' => 4,
             // 's' => 'qrl',
             // 'w' => 300,
             // 'h' => 300
@@ -22,5 +23,25 @@
         imagepng($image, $png);
     }
 ?>
-<img src=<?php echo $png;?>>
+<html>
+    <head>
+    <style>
+        img.qrcode {
+            width: 400px;
+            height: auto;
+            margin: auto;
+        }
+        div.qrcode {
+            /* width: 100%; */
+            /* align: center; */
+            background-color: green;
+        }
+    </style>    
+    </head>
+<body>
+<div class=qrcode>
+<img src=<?php echo $png;?> class=qrcode>
 <p><?php echo $url;?></p>
+</div>
+</body>
+</html>
